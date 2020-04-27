1c682438769b52ed05f2718632497baed446153e
Dylan <dehall@mitre.org>
2020-04-27 18:23:31 -0400

    remove unused import

--- src/components/DropDown/__tests__/DropDown.test.tsx
+++ src/components/DropDown/__tests__/DropDown.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import DropDown from '../DropDown';
 
 const label = 'drop down options';
