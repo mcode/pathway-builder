cb2d7db385651312c6cc300da2e4b3ce8d755b04
danlee1025 <danlee1025@gmail.com>
2020-05-09 15:29:39 -0400

    Create Pathway (#6)
    * Implemented create pathway functionality.
    
    * Removed commented out code.
    
    Co-authored-by: Dan Lee <daniellee@mitre.org>
    Co-authored-by: Noranda Brown <noranda@norandabrown.com>

--- src/components/PathwaysList/PathwaysList.tsx
+++ src/components/PathwaysList/PathwaysList.tsx
@@ -1,21 +1,64 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Pathway } from 'pathways-model';
 import { Service } from 'pathways-objects';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faEdit, faTrashAlt, faTimes } from '@fortawesome/free-solid-svg-icons';
 import {
   Button,
+  Modal,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
-  TableRow
+  TableRow,
+  TextField
 } from '@material-ui/core';
 
 import Loading from 'components/elements/Loading/Loading';
 
 import styles from './PathwaysList.module.scss';
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  paper: {
+    width: '60vw',
+    backgroundColor: theme.palette.background.paper,
+    padding: '4em',
+    outline: 0
+  },
+  textField: {
+    width: '100%'
+  },
+  inputBase: {
+    color: theme.palette.common.black,
+    borderColor: `${theme.palette.text.primary}!important`,
+    '&:hover': {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  inputLabel: {
+    color: `${theme.palette.common.black}!important`,
+    fontStyle: 'italic'
+  },
+  createButton: {
+    float: 'right',
+    width: '25%',
+    marginTop: '5em'
+  },
+  modalCloseButton: {
+    float: 'right',
+    '&:hover': {
+      cursor: 'pointer'
+    }
+  }
+}));
 
 interface PathwaysTableProps {
   pathways: Pathway[];
@@ -79,7 +122,36 @@ interface PathwaysListProps {
 }
 
 const PathwaysList: FC<PathwaysListProps> = ({ pathways, callback, service }) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [newPathwayName, setNewPathwayName] = useState('');
+  const [newPathwayDescription, setNewPathwayDescription] = useState('');
   if (service.status === 'loading') return <Loading />;
+
+  const handleClose = (): void => {
+    // closes modal and resets new pathway state
+    setOpen(false);
+    setNewPathwayName('');
+    setNewPathwayDescription('');
+  };
+
+  const createNewPathway = (): void => {
+    const newPathway = {
+      name: newPathwayName,
+      description: newPathwayDescription,
+      library: '',
+      criteria: [],
+      states: {
+        Start: {
+          label: 'Start',
+          transitions: []
+        }
+      }
+    };
+    pathways.push(newPathway);
+    handleClose();
+    callback(newPathway);
+  };
 
   return (
     <div className={styles.root}>
@@ -88,10 +160,62 @@ const PathwaysList: FC<PathwaysListProps> = ({ pathways, callback, service }) =>
         variant="contained"
         color="primary"
         startIcon={<FontAwesomeIcon icon={faPlus} />}
-        onClick={(): void => callback()}
+        onClick={(): void => setOpen(true)}
       >
         Create Pathway
       </Button>
+      <Modal className={classes.modal} open={open} onClose={handleClose}>
+        <div className={classes.paper}>
+          <div className={classes.modalCloseButton} onClick={handleClose}>
+            <FontAwesomeIcon icon={faTimes} size="lg" color="gray" />
+          </div>
+          <div>
+            <TextField
+              className={classes.textField}
+              label="Pathway Name"
+              value={newPathwayName}
+              variant="outlined"
+              onChange={(event): void => setNewPathwayName(event.target.value)}
+              InputLabelProps={{
+                className: classes.inputLabel
+              }}
+              InputProps={{
+                classes: {
+                  input: classes.inputBase,
+                  notchedOutline: classes.inputBase
+                }
+              }}
+            />
+          </div>
+          <div className={styles.textField}>
+            <TextField
+              className={classes.textField}
+              label="Pathway Description"
+              value={newPathwayDescription}
+              variant="outlined"
+              onChange={(event): void => setNewPathwayDescription(event.target.value)}
+              InputLabelProps={{
+                className: classes.inputLabel
+              }}
+              InputProps={{
+                classes: {
+                  input: classes.inputBase,
+                  notchedOutline: classes.inputBase
+                }
+              }}
+            />
+          </div>
+          <Button
+            className={classes.createButton}
+            variant="contained"
+            color="primary"
+            startIcon={<FontAwesomeIcon icon={faPlus} />}
+            onClick={createNewPathway}
+          >
+            Create
+          </Button>
+        </div>
+      </Modal>
 
       <PathwaysTable
         pathways={pathways}
