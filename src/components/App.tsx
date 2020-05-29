35f15f5e357f408893e5c2d0482acd3767500024
Benjamin Langley <blangley@mitre.org>
2020-05-29 13:41:39 -0400

    Make Tabs component generic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,16 @@
 import React, { FC, memo } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 import Header from 'components/Header';
-import PathwaysList from 'components/PathwaysList';
-import CriteriaList from 'components/CriteriaList';
 
 import BuilderRoute from './BuilderRoute';
 import ThemeProvider from './ThemeProvider';
 import { PathwayProvider } from './PathwayProvider';
 import { UserProvider } from './UserProvider';
 import { CriteriaProvider } from './CriteriaProvider';
-import { Button, makeStyles, Theme, createStyles } from '@material-ui/core';
+import Tabs from './Tabs';
+import PathwaysList from './PathwaysList';
+import CriteriaList from './CriteriaList';
 
 const App: FC = () => {
   return (
@@ -29,7 +28,12 @@ const App: FC = () => {
                 </Route>
                 <Route path="/">
                   <Header />
-                  <PathwayCriteriaTabs />
+                  <Tabs
+                    tabs={[
+                      { label: 'Pathway', component: <PathwaysList /> },
+                      { label: 'Criteria', component: <CriteriaList /> }
+                    ]}
+                  />
                 </Route>
               </Switch>
             </Router>
@@ -37,40 +41,6 @@ const App: FC = () => {
         </PathwayProvider>
       </UserProvider>
     </ThemeProvider>
-  );
-};
-
-const PathwayCriteriaTabs: FC = () => {
-  const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      label: {
-        padding: `0.2em 2em`,
-        textTransform: 'none',
-        fontSize: '1.2em',
-        fontWeight: 100
-      }
-    })
-  );
-  const styles = useStyles();
-
-  return (
-    <Tabs>
-      <TabList>
-        <Tab>
-          <Button classes={{ label: styles.label }}>Pathways</Button>
-        </Tab>
-        <Tab>
-          <Button classes={{ label: styles.label }}>Criteria</Button>
-        </Tab>
-      </TabList>
-
-      <TabPanel>
-        <PathwaysList />
-      </TabPanel>
-      <TabPanel>
-        <CriteriaList />
-      </TabPanel>
-    </Tabs>
   );
 };
 
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,43 @@
+import React, { FC, memo, ReactNode } from 'react';
+import { Tab as ReactTab, TabList, TabPanel, Tabs as ReactTabs } from 'react-tabs';
+
+import PathwaysList from 'components/PathwaysList';
+import CriteriaList from 'components/CriteriaList';
+import { Button } from '@material-ui/core';
+
+import useStyles from './styles';
+
+interface Tab {
+  label: string;
+  component: ReactNode;
+}
+
+interface TabsProp {
+  tabs: Tab[];
+}
+
+const Tabs: FC<TabsProp> = ({ tabs }) => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.root}>
+      <ReactTabs>
+        <TabList>
+          {tabs.map(tab => {
+            return (
+              <ReactTab>
+                <Button classes={{ label: styles.label }}>{tab.label}</Button>
+              </ReactTab>
+            );
+          })}
+        </TabList>
+
+        {tabs.map(tab => {
+          return <TabPanel>{tab.component}</TabPanel>;
+        })}
+      </ReactTabs>
+    </div>
+  );
+};
+
+export default memo(Tabs);
