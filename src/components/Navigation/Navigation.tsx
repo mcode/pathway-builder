9bbcf4bd169e12542ba795bd0a560ad5164fe02e
Dan Lee <danlee1025@gmail.com>
2020-06-22 23:16:44 -0400

    Added menu and export pathway to builder view.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,10 +1,13 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState, MouseEvent } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import { IconButton } from '@material-ui/core';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import { Pathway } from 'pathways-model';
+import { downloadPathway } from 'utils/builder';
 import useStyles from './styles';
 
 interface Props {
@@ -12,8 +15,17 @@ interface Props {
 }
 
 const Navigation: FC<Props> = ({ pathway }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const styles = useStyles();
   const history = useHistory();
+
+  const openMenu = useCallback((event: MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(event.currentTarget);
+  }, []);
+
+  const closeMenu = useCallback((): void => {
+    setAnchorEl(null);
+  }, []);
 
   const handleGoBack = useCallback((): void => {
     history.push('/');
@@ -21,11 +33,34 @@ const Navigation: FC<Props> = ({ pathway }) => {
 
   return (
     <nav className={styles.root}>
-      <IconButton className={styles.backButton} onClick={handleGoBack} aria-label="go back">
-        <FontAwesomeIcon icon={faChevronLeft} className={styles.backIcon} />
-      </IconButton>
+      <div>
+        <IconButton className={styles.backButton} onClick={handleGoBack} aria-label="go back">
+          <FontAwesomeIcon icon={faChevronLeft} className={styles.navigationIcons} />
+        </IconButton>
 
-      <div className={styles.pathwayName}>{pathway?.name}</div>
+        <span className={styles.pathwayName}>{pathway?.name}</span>
+      </div>
+      <IconButton onClick={openMenu} aria-controls="pathway-options-menu" aria-haspopup="true">
+        <FontAwesomeIcon icon={faEllipsisH} className={styles.navigationIcons} />
+      </IconButton>
+      <Menu
+        id="pathway-options-menu"
+        anchorEl={anchorEl}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={closeMenu}
+      >
+        <MenuItem
+          onClick={(): void => {
+            downloadPathway(pathway);
+            closeMenu();
+          }}
+        >
+          Export Pathway
+        </MenuItem>
+      </Menu>
     </nav>
   );
 };
