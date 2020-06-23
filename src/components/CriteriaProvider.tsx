f880bdaf65276fe8d9a260bcceb745b310c47434
Jack <johnfraser1219@gmail.com>
2020-06-23 16:30:33 -0400

    Default criteria added to ease builder use.

--- src/components/CriteriaProvider.tsx
+++ src/components/CriteriaProvider.tsx
@@ -9,6 +9,8 @@ import React, {
 } from 'react';
 import shortid from 'shortid';
 import { ElmStatement } from 'elm-model';
+import under2cm from 'components/Tumor-Size-2cm-and-Under.json';
+import over2cm from 'components/Tumor-Size-Over-2cm.json';
 
 interface Criteria {
   id: string;
@@ -32,41 +34,51 @@ interface CriteriaProviderProps {
   children: ReactNode;
 }
 
+function JSONtoCriteria(rawElm: string): Criteria | undefined {
+  const elm = JSON.parse(rawElm);
+  if (!elm.library?.identifier) {
+    alert('Please upload ELM file');
+    return;
+  }
+  const defaultStatementNames = [
+    'Patient',
+    'MeetsInclusionCriteria',
+    'InPopulation',
+    'Recommendation',
+    'Rationale',
+    'Errors'
+  ];
+  const elmStatements: ElmStatement[] = elm.library.statements.def;
+  const elmStatement = elmStatements.find(def => !defaultStatementNames.includes(def.name));
+  if (!elmStatement) {
+    alert('No elm statement found in that file');
+    return;
+  }
+  const newCriteria: Criteria = {
+    id: shortid.generate(),
+    label: elm.library.identifier.id,
+    version: elm.library.identifier.version,
+    modified: Date.now(),
+    elm: elm
+  };
+  return newCriteria;
+}
+
 export const CriteriaProvider: FC<CriteriaProviderProps> = memo(({ children }) => {
-  const [criteria, setCriteria] = useState<Criteria[]>([]);
+  const under2cmCriteria = JSONtoCriteria(JSON.stringify(under2cm));
+  const over2cmCriteria = JSONtoCriteria(JSON.stringify(over2cm));
+
+  let defaultCriteria: Criteria[] = [];
+  if (under2cmCriteria && over2cmCriteria) defaultCriteria = [under2cmCriteria, over2cmCriteria];
+  const [criteria, setCriteria] = useState<Criteria[]>(defaultCriteria);
 
   const addCriteria = useCallback((file: File) => {
     const reader = new FileReader();
     reader.onload = (event: ProgressEvent<FileReader>): void => {
       if (event.target?.result) {
         const rawElm = event.target.result as string;
-        const elm = JSON.parse(rawElm);
-        if (!elm.library?.identifier) {
-          alert('Please upload ELM file');
-          return;
-        }
-        const defaultStatementNames = [
-          'Patient',
-          'MeetsInclusionCriteria',
-          'InPopulation',
-          'Recommendation',
-          'Rationale',
-          'Errors'
-        ];
-        const elmStatements: ElmStatement[] = elm.library.statements.def;
-        const elmStatement = elmStatements.find(def => !defaultStatementNames.includes(def.name));
-        if (!elmStatement) {
-          alert('No elm statement found in that file');
-          return;
-        }
-        const newCriteria = {
-          id: shortid.generate(),
-          label: elm.library.identifier.id,
-          version: elm.library.identifier.version,
-          modified: Date.now(),
-          elm: elm
-        };
-        setCriteria(currentCriteria => [...currentCriteria, newCriteria]);
+        const newCriteria = JSONtoCriteria(rawElm);
+        if (newCriteria) setCriteria(currentCriteria => [...currentCriteria, newCriteria]);
       } else alert('Unable to read that file');
     };
     reader.readAsText(file);
