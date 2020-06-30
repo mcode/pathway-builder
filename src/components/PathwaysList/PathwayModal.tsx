71b3998715961d844e923fb3d0cd219adde95eec
Jack <johnfraser1219@gmail.com>
2020-06-30 15:37:10 -0400

    Allows users to edit the metadata for any existing pathway. Additionally, pathway name is now a button to access the builder view.

--- src/components/PathwaysList/PathwayModal.tsx
+++ src/components/PathwaysList/PathwayModal.tsx
@@ -16,18 +16,21 @@ import shortid from 'shortid';
 import { usePathwayContext } from 'components/PathwayProvider';
 import useStyles from './styles';
 import { createNewPathway } from 'utils/builder';
+import { Pathway } from 'pathways-model';
 
-interface NewPathwayModalProps {
+interface PathwayModalProps {
   open: boolean;
   onClose: () => void;
+  editPath?: Pathway;
 }
 
-const NewPathwayModal: FC<NewPathwayModalProps> = ({ open, onClose }) => {
+const PathwayModal: FC<PathwayModalProps> = ({ open, onClose, editPath }) => {
+  const createNewModal = !editPath;
   const styles = useStyles();
   const history = useHistory();
   const pathwayNameRef = useRef<HTMLInputElement>(null);
   const pathwayDescRef = useRef<HTMLInputElement>(null);
-  const { addPathway } = usePathwayContext();
+  const { addPathway, updatePathwayAtIndex, pathways } = usePathwayContext();
 
   const closeModal = useCallback(
     (pathwayId: string): void => {
@@ -49,6 +52,30 @@ const NewPathwayModal: FC<NewPathwayModalProps> = ({ open, onClose }) => {
     [addPathway, closeModal]
   );
 
+  const handleUpdatePathway = useCallback(
+    (event: FormEvent<HTMLFormElement>): void => {
+      event.preventDefault();
+      if (editPath) {
+        if (pathwayNameRef.current?.value) editPath.name = pathwayNameRef.current?.value;
+        if (pathwayDescRef.current?.value !== '')
+          editPath.description = pathwayDescRef.current?.value;
+        const pathwayId = editPath.id;
+        const pathwayIndex = pathways.findIndex(pathway => pathway.id === pathwayId);
+        updatePathwayAtIndex(editPath, pathwayIndex);
+        onClose();
+      }
+    },
+    [pathways, updatePathwayAtIndex, editPath, onClose]
+  );
+
+  let label = 'Pathway Name';
+  let description = 'Pathway Description';
+  if (editPath) {
+    label = editPath.name;
+    const metaDescription = editPath.description;
+    if (metaDescription && metaDescription !== '') description = metaDescription;
+  }
+
   return (
     <Dialog open={open} onClose={onClose} aria-labelledby="create-pathway" fullWidth maxWidth="md">
       <DialogTitle disableTypography>
@@ -57,23 +84,18 @@ const NewPathwayModal: FC<NewPathwayModalProps> = ({ open, onClose }) => {
         </IconButton>
       </DialogTitle>
 
-      <form onSubmit={handleCreateNewPathway}>
+      <form onSubmit={createNewModal ? handleCreateNewPathway : handleUpdatePathway}>
         <DialogContent>
           <TextField
             variant="outlined"
             autoFocus
-            label="Pathway Name"
+            label={label}
             fullWidth
-            required
+            required={createNewModal}
             inputRef={pathwayNameRef}
           />
 
-          <TextField
-            variant="outlined"
-            label="Pathway Description"
-            fullWidth
-            inputRef={pathwayDescRef}
-          />
+          <TextField variant="outlined" label={description} fullWidth inputRef={pathwayDescRef} />
         </DialogContent>
 
         <DialogActions>
@@ -83,7 +105,7 @@ const NewPathwayModal: FC<NewPathwayModalProps> = ({ open, onClose }) => {
             startIcon={<FontAwesomeIcon icon={faPlus} />}
             type="submit"
           >
-            Create
+            {createNewModal ? 'Create' : 'Update Info'}
           </Button>
         </DialogActions>
       </form>
@@ -91,4 +113,4 @@ const NewPathwayModal: FC<NewPathwayModalProps> = ({ open, onClose }) => {
   );
 };
 
-export default memo(NewPathwayModal);
+export default memo(PathwayModal);
--- src/components/PathwaysList/PathwaysTable.tsx
+++ src/components/PathwaysList/PathwaysTable.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo } from 'react';
+import React, { FC, memo, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faFileDownload, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
@@ -14,69 +14,92 @@ import {
 
 import { usePathwayContext } from 'components/PathwayProvider';
 import { downloadPathway } from 'utils/builder';
+import PathwayModal from './PathwayModal';
 
 import useStyles from './styles';
+import { Pathway } from 'pathways-model';
 
 const PathwaysTable: FC = () => {
   const styles = useStyles();
   const { pathways, deletePathway } = usePathwayContext();
+  const [open, setOpen] = useState(false);
+  const [editablePathway, setEditablePathway] = useState<Pathway>();
+
+  function openEditPathwayModal(pathway: Pathway): void {
+    setOpen(true);
+    setEditablePathway(pathway);
+  }
+
+  const closeEditPathwayModal = useCallback((): void => {
+    setOpen(false);
+  }, []);
 
   return (
-    <TableContainer className={styles.pathwayList}>
-      <Table aria-label="pathway list">
-        <TableHead>
-          <TableRow>
-            <TableCell>Pathway Name</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Last Updated</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {pathways.map(pathway => (
-            <TableRow key={pathway.id}>
-              <TableCell component="th" scope="row">
-                {pathway.name}
-              </TableCell>
-
-              <TableCell>draft</TableCell>
-              <TableCell>2 days ago</TableCell>
-
-              <TableCell align="right">
-                <Button
-                  className={styles.pathwaysListButton}
-                  color="primary"
-                  size="small"
-                  startIcon={<FontAwesomeIcon icon={faEdit} />}
-                  component={Link}
-                  to={`/builder/${encodeURIComponent(pathway.id)}`}
-                >
-                  Edit
-                </Button>
-                <Button
-                  className={styles.pathwaysListButton}
-                  color="primary"
-                  size="small"
-                  startIcon={<FontAwesomeIcon icon={faFileDownload} />}
-                  onClick={(): void => downloadPathway(pathway)}
-                >
-                  Export
-                </Button>
-                <Button
-                  color="secondary"
-                  size="small"
-                  startIcon={<FontAwesomeIcon icon={faTrashAlt} />}
-                  onClick={(): void => deletePathway(pathway.id)}
-                >
-                  Delete
-                </Button>
-              </TableCell>
+    <div>
+      <TableContainer className={styles.pathwayList}>
+        <Table aria-label="pathway list">
+          <TableHead>
+            <TableRow>
+              <TableCell>Pathway Name</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Last Updated</TableCell>
+              <TableCell></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+
+          <TableBody>
+            {pathways.map(pathway => (
+              <TableRow key={pathway.id}>
+                <TableCell component="th" scope="row">
+                  <Button
+                    className={styles.pathwaysListButton}
+                    color="primary"
+                    size="small"
+                    component={Link}
+                    to={`/builder/${encodeURIComponent(pathway.id)}`}
+                  >
+                    {pathway.name}
+                  </Button>
+                </TableCell>
+
+                <TableCell>draft</TableCell>
+                <TableCell>2 days ago</TableCell>
+
+                <TableCell align="right">
+                  <Button
+                    className={styles.pathwaysListButton}
+                    color="primary"
+                    size="small"
+                    startIcon={<FontAwesomeIcon icon={faEdit} />}
+                    onClick={(): void => openEditPathwayModal(pathway)}
+                  >
+                    Edit Info
+                  </Button>
+                  <Button
+                    className={styles.pathwaysListButton}
+                    color="primary"
+                    size="small"
+                    startIcon={<FontAwesomeIcon icon={faFileDownload} />}
+                    onClick={(): void => downloadPathway(pathway)}
+                  >
+                    Export
+                  </Button>
+                  <Button
+                    color="secondary"
+                    size="small"
+                    startIcon={<FontAwesomeIcon icon={faTrashAlt} />}
+                    onClick={(): void => deletePathway(pathway.id)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <PathwayModal open={open} onClose={closeEditPathwayModal} editPath={editablePathway} />
+    </div>
   );
 };
 
