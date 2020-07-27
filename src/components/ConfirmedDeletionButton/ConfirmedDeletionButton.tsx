3b5db0e120920f9019dd031096f4445b23a0bd92
Jack <johnfraser1219@gmail.com>
2020-07-27 12:56:13 -0400

    Replaced semantic ui Popup with material ui Popover.

--- src/components/ConfirmedDeletionButton/ConfirmedDeletionButton.tsx
+++ src/components/ConfirmedDeletionButton/ConfirmedDeletionButton.tsx
@@ -1,38 +1,74 @@
-import React, { FC, memo } from 'react';
-import withConfirmationPopup from 'components/withConfirmationPopup';
+import React, { FC, memo, useState, MouseEvent, useCallback } from 'react';
 
 import { Button } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import Popover from '@material-ui/core/Popover';
+import useStyles from './styles';
+
+import ActionButton from 'components/ActionButton';
 
 interface ConfirmedDeletionButtonProps {
-  deleteType: string;
-  deleteName: string;
   deleteId: string;
   deleteMethod: (id: string) => void;
+  deleteType?: string;
+  deleteName?: string;
 }
 
 const ConfirmedDeletionButton: FC<ConfirmedDeletionButtonProps> = ({
-  deleteType,
-  deleteName,
   deleteId,
-  deleteMethod
+  deleteMethod,
+  deleteType,
+  deleteName
 }) => {
-  const ButtonPopup = withConfirmationPopup(Button);
-  const confirmationText =
-    'Are you sure you would like to delete the ' + deleteName + ' ' + deleteType + ' ?';
+  const styles = useStyles();
+  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const displayText =
+    'Are you sure ' +
+    (deleteType && deleteMethod
+      ? 'that you would like to delete the ' + deleteName + ' ' + deleteType + ' ?'
+      : '?');
+
+  const onClickHandler = useCallback((event: MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  }, []);
+
+  const onCloseHandler = useCallback(
+    (accept?: boolean): void => {
+      if (accept) deleteMethod(deleteId);
+      setOpen(false);
+      setAnchorEl(null);
+    },
+    [deleteId, deleteMethod]
+  );
 
   return (
-    <ButtonPopup
-      onConfirm={deleteMethod}
-      onConfirmParameter={deleteId}
-      color="secondary"
-      size="small"
-      startIcon={<FontAwesomeIcon icon={faTrashAlt} />}
-      displayText={confirmationText}
-    >
-      Delete
-    </ButtonPopup>
+    <div className={styles.container}>
+      <Button
+        color="secondary"
+        size="small"
+        startIcon={<FontAwesomeIcon icon={faTrashAlt} />}
+        onClick={onClickHandler}
+      >
+        Delete
+      </Button>
+      <Popover
+        classes={{ paper: styles.paper }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <div className={styles.displayText}> {displayText} </div>
+        <div className={styles.buttons}>
+          <ActionButton size="small" type="accept" onClick={(): void => onCloseHandler(true)} />
+          <ActionButton size="small" type="decline" onClick={(): void => onCloseHandler(false)} />
+        </div>
+      </Popover>
+    </div>
   );
 };
 
