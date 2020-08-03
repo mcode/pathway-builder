964c935a331eb3f517e4f907e3227c2985a8313a
Jack <johnfraser1219@gmail.com>
2020-08-03 14:55:23 -0400

    Implemented connect node button.

--- src/components/ConnectNode/styles.tsx
+++ src/components/ConnectNode/styles.tsx
@@ -1,7 +1,30 @@
 import { makeStyles, Theme as AugmentedTheme } from '@material-ui/core/styles';
 
 export default makeStyles(
-	(theme: AugmentedTheme) => ({
-	}),
-	{ name: 'ConnectNode' }
+  (theme: AugmentedTheme) => ({
+    connectDropdown: {
+      margin: 'auto',
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'nowrap',
+      alignContent: 'center'
+    },
+    connectText: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      fontSize: '0.95em',
+      fontStyle: 'italic'
+    },
+    cancelButton: {
+      marginLeft: 'auto',
+      border: 'none',
+      alignSelf: 'center',
+      color: theme.palette.common.blueLighter,
+      fontSize: '1em',
+      fontStyle: 'italic',
+      textTransform: 'none'
+    }
+  }),
+  { name: 'ConnectNode' }
 );
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,11 +10,7 @@ import React, {
 } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faChevronLeft,
-  faChevronRight,
-  faPlus
-} from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import {
   SidebarHeader,
