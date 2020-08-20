79b6fcaa6a9ed9eb0e35779368c0479f9507d6e2
Noranda Brown <noranda@norandabrown.com>
2020-08-19 23:33:54 -0400

    Updated Immer to work in IE11

---
+++ src/immer.js
@@ -0,0 +1,4 @@
+import { enableAllPlugins } from 'immer';
+
+// enables immer to run in IE11
+enableAllPlugins();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import './immer';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { enableMapSet } from 'immer';
 
 import App from 'components/App';
 
 import './styles/index.scss';
-
-// Enable use of immer produce on maps
-enableMapSet();
 
 // Enable why did you render for development mode
 // Will automatically track all memoized components
