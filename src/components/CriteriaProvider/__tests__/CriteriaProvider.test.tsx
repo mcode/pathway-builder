c2ff88c3051df93eabd3e2c715416a19756cc42d
Reece Adamson <radamson@mitre.org>
2020-10-15 08:40:30 -0400

    lint

--- src/components/CriteriaProvider/__tests__/CriteriaProvider.test.tsx
+++ src/components/CriteriaProvider/__tests__/CriteriaProvider.test.tsx
@@ -1,21 +1,19 @@
 import { CriteriaProvider, useCriteriaContext } from '../CriteriaProvider';
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 import React, { FC } from 'react';
 
 const DisplayCriteria: FC<> = () => {
   const { criteria } = useCriteriaContext();
-  return (
-    <span>Number of Criteria: {criteria.length}</span>
-  )
-}
+  return <span>Number of Criteria: {criteria.length}</span>;
+};
 
 describe('<CriteriaProvider />', () => {
   it('provides access to criteria', () => {
     render(
       <CriteriaProvider>
-        <DisplayCriteria/>
+        <DisplayCriteria />
       </CriteriaProvider>
-    )
-    expect(screen.getByText(/^Number of Criteria:/)).toHaveTextContent('Number of Criteria: 0')
-  })
+    );
+    expect(screen.getByText(/^Number of Criteria:/)).toHaveTextContent('Number of Criteria: 0');
+  });
 });
