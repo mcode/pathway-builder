74a7f7ae8133adcfd701928a9c724e1af59e4fac
Reece Adamson <radamson@mitre.org>
2020-10-16 17:04:41 -0400

    basic pathway route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import pathwayRouter from './routes/pathway';
 
 const app = express();
 const PORT = 8000;
 app.get('/', (req, res) => res.send('Pathways Backend'));
+app.use('/pathway', pathwayRouter);
 app.listen(PORT);
