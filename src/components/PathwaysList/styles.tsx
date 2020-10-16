a0ca44c00b55dd65f951f01172836b5ee4aad54c
Reece Adamson <radamson@mitre.org>
2020-10-16 15:09:02 -0400

    reduce the table margin-top

--- src/components/PathwaysList/styles.tsx
+++ src/components/PathwaysList/styles.tsx
@@ -12,7 +12,7 @@ export default makeStyles(
       marginLeft: '20px'
     },
     pathwayList: {
-      marginTop: '2em'
+      marginTop: '1em'
     },
     pathwaysListButton: {
       marginRight: '1em'
