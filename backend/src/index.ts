bcd1a0e1af93a13e5807005909b0331b7f6f2fec
Reece Adamson <radamson@mitre.org>
2020-10-16 18:51:51 -0400

    mongodbg

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import pathwayRouter from 'routes/pathway';
+import mongoose from 'mongoose';
+import Pathway from 'models/pathway';
 
 const app = express();
 const PORT = 8000;
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
 app.get('/', (req, res) => res.send('Pathways Backend'));
 app.use('/pathway', pathwayRouter);
 app.listen(PORT);
