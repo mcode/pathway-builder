bc36bc94a6aaec5f70300ff09adf29351141b915
Reece Adamson <radamson@mitre.org>
2020-10-17 10:48:47 -0400

    enable linting

--- backend/src/routes/pathway.ts
+++ backend/src/routes/pathway.ts
@@ -4,17 +4,17 @@ import Pathway from 'models/pathway';
 const router = Router();
 
 router.get('/', (req, res) => {
-  return Pathway.find({id: "2TJDsrHux"})
+  return Pathway.find({ id: '2TJDsrHux' });
 });
 
 router.post('/', (req, res) => {
   new Pathway(req.body).save((err, product) => {
-    if (err) res.send('Error adding pathway')
-    else res.send(`Added new user ${product._id}`)
+    if (err) res.send('Error adding pathway');
+    else res.send(`Added new user ${product._id}`);
   });
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   Pathway.findById(req.params.id, (err, user) => {
     if (err) res.send('err');
     else res.send('user');
