4d64c34916173ce7687203f1873550ec509cbaf3
Reece Adamson <radamson@mitre.org>
2020-10-22 19:42:32 -0400

    works as a library

--- backend/src/extensions/mongoose.extension.ts
+++ backend/src/extensions/mongoose.extension.ts
@@ -1,5 +1,6 @@
-import QueryFindOneAndUpdateOptions from 'mongoose';
+import 'mongoose';
 
+// https://stackoverflow.com/questions/47422588/extend-typescript-definitelytyped-definition-file
 declare module 'mongoose' {
   interface QueryFindOneAndUpdateOptions {
     overwrite?: boolean;
