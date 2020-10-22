20a4ecf2ce695252bf57276cb3a373faaae859a3
Reece Adamson <radamson@mitre.org>
2020-10-22 16:32:18 -0400

    better routes

--- backend/src/routes/pathway.ts
+++ backend/src/routes/pathway.ts
@@ -7,17 +7,26 @@ router.get('/', (req, res) => {
   return Pathway.find({ id: '2TJDsrHux' });
 });
 
-router.post('/', (req, res) => {
-  new Pathway(req.body).save((err, product) => {
-    if (err) res.send('Error adding pathway');
-    else res.send(`Added new user ${product._id}`);
-  });
+router.put('/:id', (req, res) => {
+  if (req.params.id !== req.body.id) res.status(409).send('Pathway id does not match URL id');
+  Pathway.findOneAndUpdate(
+    { id: req.params.id },
+    req.body,
+    // @ts-ignore
+    { overwrite: true, new: true },
+    (err: object, product: Document | null) => {
+      if (err) res.send(err);
+      else res.status(201).send(product);
+    }
+  );
 });
 
 router.get('/:id', (req, res) => {
   Pathway.findOne({ id: req.params.id }, { _id: 0 }, (err, pathway) => {
     if (err) res.send(err);
-    else res.send(pathway);
+    if (pathway) {
+      res.status(200).send(pathway);
+    } else res.status(404).send(pathway);
   });
 });
 
