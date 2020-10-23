c8e2d45641c9fd4405a528686cf6e9f4fa80a71a
Reece Adamson <radamson@mitre.org>
2020-10-23 17:33:17 -0400

    add workspace routes

---
+++ backend/src/models/workspace.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose';
+
+const workspaceSchema = new Schema({
+  id: String,
+});
+
+workspaceSchema.index( { id: 1 }, { unique: true });
+
+const Workspace = model('Workspace', workspaceSchema);
+
+export default Workspace;
