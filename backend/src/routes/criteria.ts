91d576867450cb6b3f5d9833305237d07f04a274
Reece Adamson <radamson@mitre.org>
2020-10-28 12:50:55 -0400

    add handlers

--- backend/src/routes/criteria.ts
+++ backend/src/routes/criteria.ts
@@ -1,42 +1,28 @@
 import { Router } from 'express';
 import Criteria from 'models/criteria';
+import {
+  deleteByIdHandler,
+  getAllHandler,
+  getByIdHandler,
+  putByIdHandler,
+} from '../handlers/crudHandler';
 
 const router = Router();
 
-router.get('/', (_, res) => {
-  Criteria.find({}, (err, product) => {
-    if (err) res.status(500).send('Error getting all Criteria');
-    else res.status(200).send(product);
-  });
+router.get('/', (req, res) => {
+  getAllHandler({ model: Criteria, req: req, res });
 });
 
 router.put('/:id', (req, res) => {
-  if (req.params.id !== req.body.id) res.status(409).send('Criteria id does not match URL id');
-  Criteria.findOneAndUpdate(
-    { id: req.params.id },
-    req.body,
-    { overwrite: true, new: true, upsert: true, rawResult: true },
-    (err, product) => {
-      if (err) res.status(500).send(err);
-      else res.status(product.lastErrorObject.updatedExisting ? 200 : 201).send(product.value);
-    }
-  );
+  putByIdHandler({ model: Criteria, req: req, res: res });
 });
 
 router.delete('/:id', (req, res) => {
-  Criteria.deleteOne({ id: req.params.id }, (err) => {
-    if (err) res.status(500).send(err);
-    else res.status(200).send(`Deleted Criteria ${req.params.id}`);
-  });
+  deleteByIdHandler({ model: Criteria, req: req, res: res });
 });
 
 router.get('/:id', (req, res) => {
-  Criteria.findOne({ id: req.params.id }, { _id: 0 }, (err, Criteria) => {
-    if (err) res.status(500).send(err);
-    if (Criteria) {
-      res.status(200).send(Criteria);
-    } else res.status(404).send(Criteria);
-  });
+  getByIdHandler({ model: Criteria, req: req, res: res });
 });
 
 export default router;
