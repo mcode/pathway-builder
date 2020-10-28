af32f0084887c8d65cda81b8f6e2e4dab6058ca1
Reece Adamson <radamson@mitre.org>
2020-10-28 12:50:55 -0400

    add handlers

--- backend/src/routes/workspace.ts
+++ backend/src/routes/workspace.ts
@@ -1,42 +1,28 @@
 import { Router } from 'express';
 import Workspace from 'models/workspace';
+import {
+  deleteByIdHandler,
+  getAllHandler,
+  getByIdHandler,
+  putByIdHandler,
+} from '../handlers/crudHandler';
 
 const router = Router();
 
-router.get('/', (_, res) => {
-  Workspace.find({}, (err, product) => {
-    if (err) res.status(500).send('Error getting all Workspaces');
-    else res.status(200).send(product);
-  });
+router.get('/', (req, res) => {
+  getAllHandler({ model: Workspace, req: req, res });
 });
 
 router.put('/:id', (req, res) => {
-  if (req.params.id !== req.body.id) res.status(409).send('Workspace id does not match URL id');
-  Workspace.findOneAndUpdate(
-    { id: req.params.id },
-    req.body,
-    { overwrite: true, new: true, upsert: true, rawResult: true },
-    (err, product) => {
-      if (err) res.status(500).send(err);
-      else res.status(product.lastErrorObject.updatedExisting ? 200 : 201).send(product.value);
-    }
-  );
+  putByIdHandler({ model: Workspace, req: req, res: res });
 });
 
 router.delete('/:id', (req, res) => {
-  Workspace.deleteOne({ id: req.params.id }, (err) => {
-    if (err) res.status(500).send(err);
-    else res.status(200).send(`Deleted Workspace ${req.params.id}`);
-  });
+  deleteByIdHandler({ model: Workspace, req: req, res: res });
 });
 
 router.get('/:id', (req, res) => {
-  Workspace.findOne({ id: req.params.id }, { _id: 0 }, (err, Workspace) => {
-    if (err) res.status(500).send(err);
-    if (Workspace) {
-      res.status(200).send(Workspace);
-    } else res.status(404).send(Workspace);
-  });
+  getByIdHandler({ model: Workspace, req: req, res: res });
 });
 
 export default router;
