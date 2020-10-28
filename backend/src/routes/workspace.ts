0c5404189ad6a8d90ec3d9efa0921ab337c55954
Reece Adamson <radamson@mitre.org>
2020-10-28 13:05:33 -0400

    add helper for adding crud routes

--- backend/src/routes/workspace.ts
+++ backend/src/routes/workspace.ts
@@ -1,28 +1,9 @@
 import { Router } from 'express';
 import Workspace from 'models/workspace';
-import {
-  deleteByIdHandler,
-  getAllHandler,
-  getByIdHandler,
-  putByIdHandler,
-} from 'handlers/crudHandler';
+import { addCrudRoutes } from 'handlers/crudHandler';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  getAllHandler({ model: Workspace, req: req, res });
-});
-
-router.put('/:id', (req, res) => {
-  putByIdHandler({ model: Workspace, req: req, res: res });
-});
-
-router.delete('/:id', (req, res) => {
-  deleteByIdHandler({ model: Workspace, req: req, res: res });
-});
-
-router.get('/:id', (req, res) => {
-  getByIdHandler({ model: Workspace, req: req, res: res });
-});
+addCrudRoutes(router, Workspace);
 
 export default router;
