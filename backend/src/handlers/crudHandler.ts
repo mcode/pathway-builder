8b1ab4cc0e4bc35059daeabfa980993dbe791a48
Reece Adamson <radamson@mitre.org>
2020-10-28 12:50:55 -0400

    add handlers

---
+++ backend/src/handlers/crudHandler.ts
@@ -0,0 +1,49 @@
+import { Model, Document } from 'mongoose';
+import { Response, Request } from 'express';
+
+interface Handler<T extends Document> {
+  model: Model<T>;
+  req: Request;
+  res: Response;
+}
+
+const getAllHandler = <T extends Document>({ model, res }: Handler<T>): void => {
+  model.find({}, (err, product) => {
+    if (err) res.status(500).send('Error getting all pathways');
+    else res.status(200).send(product);
+  });
+};
+
+const putByIdHandler = <T extends Document>({ model, req, res }: Handler<T>): void => {
+  if (req.params.id !== req.body.id) res.status(409).send('Pathway id does not match URL id');
+  model.findOneAndUpdate(
+    // Issues with TypeScript generics make it hard to handle the filter query parameters elegantly
+    // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/46550
+    // https://github.com/microsoft/TypeScript/pull/30639
+    { id: req.params.id } as object,
+    req.body,
+    { overwrite: true, new: true, upsert: true, rawResult: true },
+    (err, product) => {
+      if (err) res.status(500).send(err);
+      else res.status(product.lastErrorObject.updatedExisting ? 200 : 201).send(product.value);
+    }
+  );
+};
+
+const deleteByIdHandler = <T extends Document>({ model, req, res }: Handler<T>): void => {
+  model.deleteOne({ id: req.params.id } as object, (err) => {
+    if (err) res.status(500).send(err);
+    else res.status(200).send(`Deleted pathway ${req.params.id}`);
+  });
+};
+
+const getByIdHandler = <T extends Document>({ model, req, res }: Handler<T>): void => {
+  model.findOne({ id: req.params.id } as object, { _id: 0 }, (err, pathway) => {
+    if (err) res.status(500).send(err);
+    if (pathway) {
+      res.status(200).send(pathway);
+    } else res.status(404).send(pathway);
+  });
+};
+
+export { getAllHandler, putByIdHandler, deleteByIdHandler, getByIdHandler };
--- backend/src/routes/pathway.ts
+++ backend/src/routes/pathway.ts
@@ -1,42 +1,28 @@
 import { Router } from 'express';
 import Pathway from 'models/pathway';
+import {
+  getAllHandler,
+  putByIdHandler,
+  deleteByIdHandler,
+  getByIdHandler,
+} from '../handlers/crudHandler';
 
 const router = Router();
 
-router.get('/', (_, res) => {
-  Pathway.find({}, (err, product) => {
-    if (err) res.status(500).send('Error getting all pathways');
-    else res.status(200).send(product);
-  });
+router.get('/', (req, res) => {
+  getAllHandler({ model: Pathway, req: req, res });
 });
 
 router.put('/:id', (req, res) => {
-  if (req.params.id !== req.body.id) res.status(409).send('Pathway id does not match URL id');
-  Pathway.findOneAndUpdate(
-    { id: req.params.id },
-    req.body,
-    { overwrite: true, new: true, upsert: true, rawResult: true },
-    (err, product) => {
-      if (err) res.status(500).send(err);
-      else res.status(product.lastErrorObject.updatedExisting ? 200 : 201).send(product.value);
-    }
-  );
+  putByIdHandler({ model: Pathway, req: req, res: res });
 });
 
 router.delete('/:id', (req, res) => {
-  Pathway.deleteOne({ id: req.params.id }, (err) => {
-    if (err) res.status(500).send(err);
-    else res.status(200).send(`Deleted pathway ${req.params.id}`);
-  });
+  deleteByIdHandler({ model: Pathway, req: req, res: res });
 });
 
 router.get('/:id', (req, res) => {
-  Pathway.findOne({ id: req.params.id }, { _id: 0 }, (err, pathway) => {
-    if (err) res.status(500).send(err);
-    if (pathway) {
-      res.status(200).send(pathway);
-    } else res.status(404).send(pathway);
-  });
+  getByIdHandler({ model: Pathway, req: req, res: res });
 });
 
 export default router;
