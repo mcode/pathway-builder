66d1aae60a765205d1594f12bf40995228fdeb45
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-25 12:23:22 +0000

    Deploy mcode/pathway-builder to github.com/mcode/pathway-builder.git:gh-pages

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 CONFIG = {
   mapper: null, // mapper name from fhir-mapper -- allows mapping of incoming FHIR resources
   cqlToElmWebserviceUrl: 'https://cql-translation-service-zfk47ib3kq-uc.a.run.app/cql/translator?annotations=true&result-types=true',
-    'http://moonshot-dev.mitre.org:8080/cql/translator?annotations=true&result-types=true',
   pathwaysService: 'https://mcode.github.io/pathways/static/pathways/',
   pathwaysBackend: 'http://localhost:8000',
   demoPathwaysService: './static/pathways/',
