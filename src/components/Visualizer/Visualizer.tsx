ea2db741c5b02d606879906078d78b99b1fac46d
kghoreshi <keeyan.ghoreshi@uconn.edu>
2020-12-01 16:20:26 -0500

    allow pasting bundles

--- src/components/Visualizer/Visualizer.tsx
+++ src/components/Visualizer/Visualizer.tsx
@@ -1,12 +1,16 @@
-import React, { FC, memo } from 'react';
-import example from './exampleTest.json';
+import React, { ChangeEvent, FC, memo, useState } from 'react';
 import { Graphviz } from 'graphviz-react';
-import { ActivityDefinition, Bundle, PlanDefinition } from 'fhir-objects';
+import { ActivityDefinition, Bundle, PlanDefinition, Library } from 'fhir-objects';
 
 interface TransitionData {
   [key: string]: string | number;
 }
 const Visualizer: FC = () => {
+  const [cpgString, setCpgString] = useState<string>('');
+  const _setCpgString = (event: ChangeEvent<{ value: string }>): void => {
+    const value = event.target.value;
+    setCpgString(value);
+  };
   const makeTransition = (fromState: string, toState: string): string => {
     const transitionData: TransitionData = {
       id: fromState + '_' + toState,
@@ -21,76 +25,88 @@ const Visualizer: FC = () => {
     const transitionAsDOT = '"' + fromState + '" -> "' + toState + '" [' + transitionParams + ']';
     return transitionAsDOT;
   };
-  const cpg = example as Bundle;
-  const transitions: string[] = [];
-  const resources = cpg.entry
-    .map(state => {
-      console.log(state);
-      const resource = state.resource;
-      if (resource && resource.id && resource.resourceType) {
-        const fromState = resource.id;
-        let label = resource.resourceType;
-        console.log(label);
-        if (label === 'PlanDefinition') {
-          console.log('WE B');
-          const planDef = resource as PlanDefinition;
-          label += `:\n${planDef.title}`;
-          const planTransitions = planDef.action
-            .map(action => {
-              const toState = action.definitionCanonical?.split('/').pop();
-              if (toState) {
-                return makeTransition(fromState, toState);
-              } else {
-                return '';
-              }
-            })
-            .join('\n');
-          transitions.push(planTransitions);
-
-          if (planDef.library) {
-            const libraryTransitions = planDef.library
-              .map(library => {
-                return makeTransition(fromState, library);
+  const makeGraph = (cpgString: string): string => {
+    const cpg = JSON.parse(cpgString) as Bundle;
+    const transitions: string[] = [];
+    const resources = cpg.entry
+      .map(state => {
+        console.log(state);
+        const resource = state.resource;
+        if (resource && resource.id && resource.resourceType) {
+          const fromState = resource.id;
+          let label = resource.resourceType;
+          if (label === 'PlanDefinition') {
+            const planDef = resource as PlanDefinition;
+            label += `: ${planDef.id}\\n${planDef.title}`;
+            const planTransitions = planDef.action
+              .map(action => {
+                const toState = action.definitionCanonical?.split('/').pop();
+                if (toState) {
+                  return makeTransition(fromState, toState);
+                } else {
+                  return '';
+                }
               })
               .join('\n');
+            transitions.push(planTransitions);
 
-            transitions.push(libraryTransitions);
+            if (planDef.library) {
+              const libraryTransitions = planDef.library
+                .map(library => {
+                  return makeTransition(fromState, library);
+                })
+                .join('\n');
+
+              transitions.push(libraryTransitions);
+            }
+          } else if (label === 'ActivityDefinition') {
+            const activityDef = resource as ActivityDefinition;
+            label += `: ${activityDef.id} \\n${activityDef.description}`;
+          } else if (label === 'Library') {
+            const lib = resource as Library;
+            console.log(lib);
+            label += `: ${lib.id} \\n${lib.title}`;
           }
-        } else if (label === 'ActivityDefinition') {
-          const activityDef = resource as ActivityDefinition;
-          label += `:\n${activityDef.title}`;
+          const node: { [key: string]: string } = {
+            id: resource.id,
+            shape: 'record',
+            style: 'rounded,filled',
+            fillcolor: 'White',
+            penwidth: '2',
+            fontcolor: 'Black',
+            label: label
+          };
+          const nodeParams = Object.keys(node)
+            .map(key => `${key} = "${node[key]}"`)
+            .join(', ');
+
+          const nodeAsDOT = '"' + resource.id + '" [' + nodeParams + ']';
+          return nodeAsDOT;
+        } else {
+          return '';
         }
-        const node: { [key: string]: string } = {
-          id: resource.id,
-          shape: 'record',
-          style: 'rounded,filled',
-          fillcolor: 'White',
-          penwidth: '2',
-          fontcolor: 'Black',
-          label: label
-        };
-        const nodeParams = Object.keys(node)
-          .map(key => `${key} = "${node[key]}"`)
-          .join(', ');
+      })
+      .join('\n');
 
-        const nodeAsDOT = '"' + resource.id + '" [' + nodeParams + ']';
-        return nodeAsDOT;
-      } else {
-        return '';
-      }
-    })
-    .join('\n');
-
-  const graphTransitions = transitions.join('\n');
-  console.log(graphTransitions);
-  let graph = 'digraph G {';
-  graph += resources;
-  graph += graphTransitions;
-  graph += '}';
-
+    const graphTransitions = transitions.join('\n');
+    console.log(graphTransitions);
+    let graph = 'digraph G {';
+    graph += resources;
+    graph += graphTransitions;
+    graph += '}';
+    return graph;
+  };
+  let graph = '';
+  try {
+    graph = makeGraph(cpgString);
+  } catch {
+    console.log('Invalid JSON: Please paste a CPG bundle into the textbox');
+    graph = '';
+  }
   return (
     <div>
-      <Graphviz dot={graph}></Graphviz>
+      <textarea value={cpgString} onChange={_setCpgString}></textarea>
+      {graph && <Graphviz dot={graph}></Graphviz>}
     </div>
   );
 };
