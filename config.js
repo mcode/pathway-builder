9fafbe4b1c654e27cdf31764216a8a3698afec46
Dylan Hall <dehall@mitre.org>
2021-02-05 15:38:46 -0500

    update cql-to-elm webservice url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 CONFIG = {
   mapper: null, // mapper name from fhir-mapper -- allows mapping of incoming FHIR resources
-  cqlToElmWebserviceUrl: 'https://cql-translation-service-zfk47ib3kq-uc.a.run.app/cql/translator?annotations=true&result-types=true',
+  cqlToElmWebserviceUrl: 'https://cql-translation-service.herokuapp.com/cql/translator?annotations=true&result-types=true',
   pathwaysService: 'https://mcode.github.io/pathways/static/pathways/',
   pathwaysBackend: 'http://localhost:8000',
   demoPathwaysService: './static/pathways/',
